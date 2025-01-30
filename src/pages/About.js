03db0792af59b4ecda1e7b827d7eb88151b1367c
Rosier Anderson <rosieranderson@Rosiers-Mac-mini.local>
2025-01-29 19:42:23 -0500

    first commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,31 +5,48 @@ import './about.css'
 
 export default function About() {
   return (
-    <section className='about-container'>
+    <article className='about-container'>
         <figure className='about-image-container'>
-             <img src='https://ajaykumar2543.github.io/Responsive-plant-store-website-/about.jpg' 
+             <img 
+                  src='https://ajaykumar2543.github.io/Responsive-plant-store-website-/about.jpg' 
                   alt='A beautiful plant in a stylish container' 
-                  className='about-image'/>
+                  className='about-image'
+             />
+            
         </figure>
-         <div className='about-content'>
-            <h4 className='about-heading'>Why Choose Us?</h4>
+
+        <div className='about-content'>
+            <h2 className='about-heading'>Why Choose Us?</h2>
             <p className='about-description'>
-                We have 4000+ unbiased reviews and our <br/> 
+                We have over <strong>4000+ unbiased reviews</strong>, and our <br/> 
                 customers trust our plant process and delivery <br/>  
-                service every time
+                service every time.
             </p>
-            <div className='about-features'>
+
+            <section className='features'>
+              
                 <ul className='features-list'>
-                    <li className='feature-item'> <BiSolidCheckboxChecked /> We always deliver on time.</li>
-                    <li className='feature-item'> <BiSolidCheckboxChecked /> 100% money back guaranteed.</li>
-                    <li className='feature-item'> <BiSolidCheckboxChecked /> We always come over for a check-up after sale.</li>
-                    <li className='feature-item'> <BiSolidCheckboxChecked /> We give you guides to protect and care for your plants.</li>
+                    <li className='feature-item'> 
+                        <BiSolidCheckboxChecked className='feature-list-icon'/> We always deliver on time.
+                    </li>
+                    <li className='feature-item'> 
+                        <BiSolidCheckboxChecked className='feature-list-icon'/> 100% money-back guaranteed.
+                    </li>
+                    <li className='feature-item'> 
+                        <BiSolidCheckboxChecked className='feature-list-icon'/> We always check up after a sale.
+                    </li>
+                    <li className='feature-item'> 
+                        <BiSolidCheckboxChecked className='feature-list-icon'/> We provide guides to care for your plants.
+                    </li>
                 </ul>
-            </div>
+            </section>
+
             <div className='about-button-container'>
-                <button className='shop-button'> Shop Now <LuArrowDownRight/></button>
+                <a href='/shop' className='shop-link'>
+                    <button className='shop-button'> Shop Now <LuArrowDownRight/></button>
+                </a>
             </div> 
-         </div>
-    </section>
+        </div>
+    </article>
   )
 }
