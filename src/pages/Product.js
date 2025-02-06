55f8df83ff51ae8d67c8629e7c8553a756b4f483
Rosier Anderson <rosieranderson@Rosiers-Mac-mini.local>
2025-02-06 01:28:18 -0500

    Fixing the Product componnent page

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,33 +1,82 @@
-import React from 'react';
+import React from "react";
 import { IoBag } from "react-icons/io5";
 import '../styles/product.css'
 
 export default function Product() {
+  const productsList = [
+    {
+      id: 1,
+      name: "Cacti Plant",
+      img: "https://ajaykumar2543.github.io/Responsive-plant-store-website-/product1.png",
+      price: "$10.99",
+    },
+    {
+      id: 2,
+      name: "Cacti Plant",
+      img: "https://ajaykumar2543.github.io/Responsive-plant-store-website-/product2.png",
+      price: "$10.99",
+    },
+    {
+      id: 3,
+      name: "Cactus Plant",
+      img: "https://ajaykumar2543.github.io/Responsive-plant-store-website-/product3.png",
+      price: "$10.99",
+    },
+    {
+      id: 4,
+      name: "Aloe Vera Plant",
+      img: "https://ajaykumar2543.github.io/Responsive-plant-store-website-/product4.png",
+      price: "$10.99",
+    },
+    {
+      id: 5,
+      name: "Succulent Plant",
+      img: "https://ajaykumar2543.github.io/Responsive-plant-store-website-/product5.png",
+      price: "$10.99",
+    },
+    {
+      id: 6,
+      name: "Succelent Plant",
+      img: "https://ajaykumar2543.github.io/Responsive-plant-store-website-/product6.png",
+      price: "$10.99",
+    },
+  ];
+
   return (
     <section aria-labelledby="products-heading" className="products">
       {/* Section Title */}
       <header className="products-header">
-        <h2 id="products-heading">Check Out Our <br />  Products</h2>
+        <h2 id="products-heading">
+          Check Out Our <br /> Products
+        </h2>
         <p>
           Here are some selected plants from our showroom, all <br />
-       <span className='second-line'>are in excellent shape. Buy and enjoy the best quality.</span>
+          <span className="second-line">
+            are in excellent shape. Buy and enjoy the best quality.
+          </span>
         </p>
       </header>
 
       {/* Product List */}
+
       <section className="product-list">
-        <article className="product-card">
-          <figure>
-            <img src="/home" alt="Cacti Plant" />
-           
-          </figure>
-          <div className="product-info">
-            <p className="product-price">$10.99</p>
-            <button className="add-to-cart" aria-label="Add Cacti Plant to cart">
-              <IoBag />
-            </button>
-          </div>
-        </article>
+        {productsList.map(({ id, name, price, img }) => (
+          <article key={id} className="product-card">
+            <figure className="product-figure">
+              <img src={img} alt={name} className="product-img"/>
+            </figure>
+            <div className="product-info">
+              <strong><p className="product-name">{name}</p></strong>
+             <strong><p className="product-price">{price}</p></strong>
+              <button
+                className="add-to-cart"
+                aria-label={`Add ${name} to cart`}
+              >
+                <IoBag className="add-to-card-icon" />
+              </button>
+            </div>
+          </article>
+        ))}
       </section>
     </section>
   );
